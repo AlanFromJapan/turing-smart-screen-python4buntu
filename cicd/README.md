4e5844a8a1a7bff906abc725a5c8a22ea5b9ded5
AlanFromJapan <alanfromjapan@gmail.com>
2025-11-08 17:46:27 +0900

    Added CICD local to release file locally

---
+++ cicd/README.md
@@ -0,0 +1,2 @@
+Icon appicon.png was "jean_victor_balin_icon_monitoring.png" is Public Domain downloaded here 
+https://freesvg.org/computer-monitoring-icon-vector-illustration
---
+++ cicd/release-local.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Create a folder in ~/AppImage if it doesn't exist
+mkdir -p ~/AppImage/turing-monitor
+
+# Copy current code to that folder
+rsync -av --exclude='.git' --exclude='logs' ~/Git/turing-smart-screen-python4buntu/ ~/AppImage/turing-monitor/
+
+#make it autostart
+mkdir -p ~/.config/autostart
+cp ~/AppImage/turing-monitor/cicd/turing-monitor.desktop ~/.config/autostart/
+
+#that's it!
+echo "Local release prepared in ~/AppImage/turing-monitor"
